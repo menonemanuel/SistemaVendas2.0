a7a88faf4ff4857a4ca51b2288aeb2fd897c2103
EMANUEL MENON <emanuel.4966@aluno.pr.senac.br>
2023-10-02 17:04:21 -0300

    sistema vendas 2.0

---
+++ src/main/java/com/mycompany/sistema_vendas/Sistema_Vendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_vendas;
+
+/**
+ *
+ * @author emanuel.4966
+ */
+public class Sistema_Vendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
